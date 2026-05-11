2d34160d6a3f95ba73fb917667eaa8762d190240
MylesSchool <22macmyl@ths.school.nz>
2026-05-12 00:05:42 +1200

    First Items and Datagen

---
+++ src/main/java/com/mohigster/morefeatures/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.mohigster.morefeatures.item;
+
+import com.mohigster.morefeatures.MoreFeatures;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MoreFeatures.MODID);
+
+    // Item registration. JSON files are generated with datagen. See MoreFeaturesDataGen and ModModelProvider.
+
+    // Texture files and language data still must be manually added.
+
+    // This mod has no theme and therefore may add a large number of items. For convenience, Items should be
+    // split into categories headed by comments, e.g. // Aluminium items.
+
+    // While this seems useless now, future me will be grateful.
+
+    // Aluminium items
+    public static final DeferredItem<Item> RAW_ALUMINIUM = ITEMS.registerSimpleItem("raw_aluminium");
+    public static final DeferredItem<Item> ALUMINIUM_INGOT = ITEMS.registerSimpleItem("aluminium_ingot");
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
